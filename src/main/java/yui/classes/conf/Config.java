9044464ad88174ab7edbed0fdc07d74c1e3e94a0
mindhaq <OaDy[6Zu>
2012-05-10 17:41:00 +0200

    started implementation of dependency sorter

--- src/main/java/yui/classes/conf/Config.java
+++ src/main/java/yui/classes/conf/Config.java
@@ -2,42 +2,97 @@ package yui.classes.conf;
 
 import java.util.*;
 
+
 public class Config {
   public final String version;
-  
+
   private final Map<String, Module> moduleMap = new HashMap<String, Module>();
-  
+
   public Config(String version) {
     this.version = version;
   }
-  
+
   public Module getModule(String name) {
     if (!moduleMap.containsKey(name)) {
       throw new IllegalArgumentException("module " + name + " not configured.");
     }
-    
+
     return moduleMap.get(name);
   }
-  
+
   public void addModule(Module module) {
     moduleMap.put(module.getName(), module);
   }
-  
+
   public void addModules(Map<String, Module> moduleMap) {
     this.moduleMap.putAll(moduleMap);
   }
-  
+
   public int getModuleCount() {
     return moduleMap.size();
   }
 
-  public Combo comboFor(boolean loadOptional, String... modules) {
+  /**
+   * Create combo for selected modules, without optionals.
+   * @param modules
+   * @return
+   */
+  public Combo comboFor(String... modules) {
+    return comboFor(false, modules);
+  }
+
+  /**
+   * Create combo for selected modules
+   * @param modules
+   * @return
+   */
+  public Combo comboForWithOptionals(String... modules) {
+    return comboFor(true, modules);
+  }
+
+  private Combo comboFor(boolean loadOptional, String... modules) {
     Combo combo = new Combo();
     combo.setLoadOptional(loadOptional);
 
     addModules(combo, modules);
+    sortModules(combo);
 
     return combo;
+  }
+
+  private void sortModules(Combo combo) {
+    List<Module> scripts = combo.getScripts();
+    boolean unsorted = true;
+    int i = 0;
+sort:
+    while ((i < 200) && unsorted) {
+      unsorted = false; // assume it stays that way
+      i++;
+
+      for (Module module : scripts) {
+        if (module.getAfter().isEmpty()) {
+          continue;
+        }
+
+        for (String afterModuleName : module.getAfter()) {
+          Module afterModule = getModule(afterModuleName);
+          int afterPos = scripts.indexOf(afterModule);
+          int modulePos = scripts.indexOf(module);
+
+          if (modulePos < afterPos) {
+            scripts.remove(afterModule);
+            scripts.add(modulePos, afterModule);
+            unsorted = true;
+            continue sort;
+          }
+        }
+      }
+    }
+
+    if (i >= 200) {
+      throw new RuntimeException(
+        "didn't finish sorting after 200 iterations; there seems to be a circular dependency.");
+    }
   }
 
   private void addModules(Combo combo, String[] modules) {
@@ -46,13 +101,18 @@ public class Config {
       if (combo.hasModule(module)) {
         continue;
       }
-      
+
       addModules(combo, module.getRequires());
       if (combo.isLoadOptional()) {
         addModules(combo, module.getOptional());
       }
-      
-      combo.addModule(module);
+
+      // add rollup dependencies
+      if (!module.getSupersedes().isEmpty()) {
+        addModules(combo, module.getSupersedes());
+      } else {
+        combo.addModule(module);
+      }
     }
   }
 
--- src/test/java/yui/classes/conf/ConfigTest.java
+++ src/test/java/yui/classes/conf/ConfigTest.java
@@ -1,18 +1,23 @@
 package yui.classes.conf;
 
+import org.apache.commons.lang3.StringUtils;
+import org.junit.Ignore;
 import org.junit.Test;
-
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import static org.hamcrest.MatcherAssert.assertThat;
 import static org.hamcrest.Matchers.*;
 
+
 public class ConfigTest {
-  
-  private final Config config = new ConfigLoader().getConfig("3.4.1");
-  
+  private final Config config = new ConfigLoader().getConfig("3.5.0");
+
+  private static final Logger LOG = LoggerFactory.getLogger(ConfigTest.class);
+
   @Test
   public void should_load_combo_with_no_dependency() {
-    Combo combo = config.comboFor(true, "yui");
-    
+    Combo combo = config.comboForWithOptionals("yui");
+
     assertThat(combo, is(not(nullValue())));
     assertScripts(combo, "yui");
     assertThat(combo.getStyles(), hasSize(0));
@@ -20,14 +25,50 @@ public class ConfigTest {
 
   @Test
   public void should_load_combo_with_simple_dependency() {
-    Combo combo = config.comboFor(true, "yui-log");  // requires yui-base
+    Combo combo = config.comboForWithOptionals("yui-log"); // requires yui-base
 
     assertThat(combo, is(not(nullValue())));
     assertScripts(combo, "yui-base", "yui-log");
     assertThat(combo.getStyles(), hasSize(0));
   }
 
+  @Test
+  public void should_load_attribute_base_with_dependencies() {
+    Combo combo = config.comboFor("attribute-base");
+
+    assertThat(combo, is(not(nullValue())));
+    assertScripts(combo, "yui-base", "attribute-core", "oop", "event-custom-base", "event-custom-complex",
+      "attribute-events", "attribute-extras", "attribute-base");
+    assertThat(combo.getStyles(), hasSize(0));
+  }
+
+  @Ignore
+  @Test
+  public void should_load_autocomplete_with_dependencies_and_styles() {
+    Combo combo = config.comboFor("autocomplete");
+
+    assertThat(combo, is(not(nullValue())));
+    assertStyles(combo, "autocomplete-list", "widget-base");
+    assertScripts(combo, "yui-base", "array-extras", "attribute-core", "base-core", "oop", "event-custom-base",
+      "event-custom-complex", "attribute-events", "attribute-extras", "attribute-base", "attribute-complex",
+      "base-base", "base-build", "escape", "features", "dom-core", "dom-base", "selector-native", "selector",
+      "node-core", "node-base", "event-base", "event-synthetic", "event-focus", "event-valuechange",
+      "autocomplete-base", "autocomplete-sources", "intl-base", "intl", "lang/autocomplete-list",
+      "skin-sam-autocomplete-list", "event-resize", "dom-style", "dom-screen", "node-screen", "selector-css2",
+      "selector-css3", "node-style", "pluginhost-base", "pluginhost-config", "node-pluginhost", "shim-plugin",
+      "skin-sam-widget-base", "base-pluginhost", "classnamemanager", "widget-base", "widget-htmlparser", "widget-skin",
+      "event-delegate", "node-event-delegate", "widget-uievents", "widget-position", "widget-position-align",
+      "autocomplete-list", "autocomplete-list-keys", "autocomplete-plugin");
+  }
+
+  @Test
+  public void should_process_after() {
+    Combo combo = new Combo();
+  }
+
   private void assertScripts(Combo combo, String... expectedModules) {
+    LOG.info("expect: [" + StringUtils.join(expectedModules, ", ") + "]");
+    LOG.info("actual: " + StringUtils.join(combo.getScripts()));
     assertThat(combo.getScripts(), contains(modulesFor(expectedModules)));
   }
 
